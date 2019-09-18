4005bfd1c5b3123716f46492db85db5e25e28cb3
Hoegsgaard <mathiashoegsgaard@gmail.com>
2019-09-18 14:59:59 +0200

    Application config og endpoint

---
+++ src/main/java/AppConfig.java
@@ -0,0 +1,7 @@
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+//Lånt fra github: diplomit-dtu
+//Defines api root
+@ApplicationPath("/rest")
+public class AppConfig extends Application {
+}
