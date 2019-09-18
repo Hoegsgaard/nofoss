b3312268cca8a2c6eb3d90f572b047d5c02778cf
Hoegsgaard <mathiashoegsgaard@gmail.com>
2019-09-18 13:49:54 +0200

    Initial commit

---
+++ src/main/java/main.java
@@ -0,0 +1,38 @@
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+import java.io.File;
+
+public class main{
+    public static void main(String[] arc){
+        Tomcat tomcat = new Tomcat();
+        tomcat.setBaseDir("temp");
+        String port = System.getenv("PORT"); //Til Heroku
+        if(port == null){
+            port = "8080";
+        }
+
+        tomcat.setPort(Integer.parseInt(port));
+        tomcat.getConnector();
+
+        tomcat.addWebapp("/", new File("src/main/webapp").getAbsolutePath());
+
+        try{
+            tomcat.start();
+        } catch(LifecycleException e){
+            e.printStackTrace();
+        }
+
+        tomcat.getServer().await();
+    }
+    private String text = "Hello JSON-world";
+
+    //Names MUST match property name for serialization to work!
+    public String getText() {
+        return text;
+    }
+
+    //Names MUST match property name for serialization to work!
+    public void setText(String text) {
+        this.text = text;
+    }
+}
