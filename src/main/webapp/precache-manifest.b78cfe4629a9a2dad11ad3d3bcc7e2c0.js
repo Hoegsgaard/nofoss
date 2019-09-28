0f383dda920b20589bf3672dc3698ff248d5ce83
Simon J <Neelix548@hotmail.com>
2019-09-28 13:22:51 +0200

    Works locally, how about Heroku? :)

---
+++ src/main/webapp/precache-manifest.b78cfe4629a9a2dad11ad3d3bcc7e2c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b70138599f1d34743106f4c709e2da23",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0ad3223b510493e0d90",
+    "url": "/static/css/main.e4c98390.chunk.css"
+  },
+  {
+    "revision": "cfc0301f4ef85e0df567",
+    "url": "/static/js/2.7b23b976.chunk.js"
+  },
+  {
+    "revision": "e0ad3223b510493e0d90",
+    "url": "/static/js/main.377541de.chunk.js"
+  },
+  {
+    "revision": "faf3d913dea1519336c1",
+    "url": "/static/js/runtime~main.0b761a9c.js"
+  }
+]);
