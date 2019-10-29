c986db6f8ec30c0e90d98a28304da9e204b33afa
hogsgaard <brejning16>
2019-10-29 13:37:29 +0100

    Layout på opret køretøj

--- web/src/components/CreateVehicle.js
+++ web/src/components/CreateVehicle.js
@@ -1,45 +1,70 @@
 import {observer} from "mobx-react";
-import {FormControl, InputGroup} from "react-bootstrap";
+import {Col, Container, FormControl, InputGroup, Row} from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import React from "react";
 import {vehicleStore} from "../stores/VehicleStore";
 
-export const CreateVehicle = observer(()=>{
+export const CreateVehicle = observer(() => {
     return (
-        <section>
-            <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                    <InputGroup.Text id="vehicleBrand">Brand</InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                    aria-label="Brand"
-                    aria-describedby="inputGroup-sizing-default"
-                    value = {vehicleStore.newBrand} onChange={(e)=>vehicleStore.newBrand=e.target.value}
-                />
-            </InputGroup>
-            <br />
-            <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                    <InputGroup.Text id="vehicleName">Name</InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                    aria-label="Name"
-                    aria-describedby="inputGroup-sizing-default"
-                    value = {vehicleStore.newName}  onChange={(e)=>vehicleStore.newName=e.target.value}
-                />
-            </InputGroup>
-            <br />
-            <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                    <InputGroup.Text id="vehiclePrice">Price</InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                    aria-label="Price"
-                    aria-describedby="inputGroup-sizing-default"
-                    value = {vehicleStore.newPrice} onChange={(e)=>vehicleStore.newPrice=e.target.value}
-                />
-            </InputGroup>
-            <Button onClick={() => vehicleStore.vehicles.push({brand:vehicleStore.newBrand,name:vehicleStore.newName,price:vehicleStore.newPrice})}>Opret Køretøj</Button>
-        </section>
+        <div>
+            <Container>
+                <Row><Col md={{span: 6, offset: 3}}
+                          style={{
+                              background: '#fff',
+                              padding: '20pt',
+                              border: '1px solid',
+                              borderColor: '#d8dee2',
+                              borderRadius: "5pt"
+                          }}>
+                    <Container>
+                        <Row><Col md={{span: 10, offset: 1}}>
+                            <text>Brand</text>
+                            <InputGroup className="mb-3">
+                                <FormControl
+                                    aria-label="Default"
+                                    aria-describedby="inputGroup-sizing-default"
+                                    value={vehicleStore.newBrand}
+                                    onChange={(e) => vehicleStore.newBrand = e.target.value}
+                                />
+                            </InputGroup></Col></Row>
+
+                        <Row><Col md={{span: 10, offset: 1}}>
+                            <text>Weight</text>
+                            <InputGroup className="mb-3">
+                                <FormControl
+                                    aria-label="Default"
+                                    aria-describedby="inputGroup-sizing-default"
+                                    value={vehicleStore.newWeight}
+                                    onChange={(e) => vehicleStore.newWeight = e.target.value}
+                                />
+                            </InputGroup></Col></Row>
+
+                        <Row><Col md={{span: 10, offset: 1}}>
+                            <text>Fuel</text>
+                            <InputGroup className="mb-3">
+                                <FormControl
+                                    aria-label="Default"
+                                    aria-describedby="inputGroup-sizing-default"
+                                    value={vehicleStore.newFuel} onChange={(e) => vehicleStore.newFuel = e.target.value}
+                                />
+                            </InputGroup></Col></Row>
+
+                        <Row><Col md={{span: 10, offset: 1}}>
+                            <Button onClick={() => vehicleStore.vehicles.push({
+                                brand: vehicleStore.newBrand,
+                                name: vehicleStore.newWeight,
+                                price: vehicleStore.newFuel
+                            })} style={{
+                                width: '100%',
+                                background: '#637724',
+                                borderColor: '#637724'
+                            }}>Opret Køretøj</Button>
+                        </Col></Row>
+                    </Container>
+                </Col></Row>
+            </Container>
+        </div>
+
+
     );
 })
