b8fc296f2fc685a056abadacf605a002dc6b876e
Mathias L. H <s144229@student.dtu.dk>
2019-11-12 13:24:18 +0100

    Users can now search for vehicles.

--- web/src/components/SearchVehicleNew.js
+++ web/src/components/SearchVehicleNew.js
@@ -10,19 +10,19 @@ import Slider from "@material-ui/core/Slider/Slider";
 import Typography from "@material-ui/core/Typography/Typography";
 import Checkbox from "@material-ui/core/Checkbox/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import {observer} from "mobx-react";
 
-export const SearchVehicleNew = () => {
+export const SearchVehicleNew = observer(() => {
 
     const [value, setValue] = React.useState([0, 100]);
-    const brandArray = vehicleStore.brands
-    const cardArray = []
+    //const brandArray = vehicleStore.brands
 
     /*For each vehicle we insert data in arrays to do further work it*/
-    vehicleStore.vehicles.map(vehicle => (
+    //vehicleStore.vehicles.map(vehicle => (
 
         /*Here should be a filter to only insert the cards passing the filter into the cardArray*/
-        cardArray.push(<CarCard Car={vehicle}/>)
-    ));
+   //     cardArray.push(<CarCard Car={vehicle}/>)
+    //));
 
     /*Contains checkbox boolean setting TODO: implement these with brandArray*/
     const [state, setState] = React.useState({
@@ -40,18 +40,30 @@ export const SearchVehicleNew = () => {
     };
 
     /*TODO: change checkboxChange reference and checked reference to {brand}-object instead of hardcoded string.  */
-    const checkBoxArray = brandArray.map(brand => (
-        <FormControlLabel
+    const checkBoxArray = vehicleStore.brands.map((brand,key) =>
+        <FormControlLabel key={key}
             control={
                 <Checkbox
-                    checked={true}
-                    onChange={checkboxChange(brand)}
+                    //checked={true}
+                    //onChange={checkboxChange(brand)}
                     value={brand}
+                    onChange = {(e) => {
+                        if(e.target.checked){
+                            vehicleStore.selectedBrands.push(brand)
+                            console.log(vehicleStore.selectedBrands)
+                        }else{
+                            var index = vehicleStore.selectedBrands.indexOf(e.target.value);
+                            if (index > -1) {
+                                vehicleStore.selectedBrands.splice(index, 1);
+                            }
+                        }
+                        }
+                    }
                 />
             }
             label={brand}
         />
-    ))
+    )
 
     return (
         <Container>
@@ -65,6 +77,7 @@ export const SearchVehicleNew = () => {
                         <FormControl
                             aria-label="Default"
                             aria-describedby="inputGroup-sizing-default"
+                            onChange={ (e) => vehicleStore.searchName = e.target.value}
                         />
                     </InputGroup>
 
@@ -83,6 +96,7 @@ export const SearchVehicleNew = () => {
                         <FormControl
                             aria-label="Default"
                             aria-describedby="inputGroup-sizing-default"
+                            onChange={(e) => vehicleStore.searchWeight = e.target.value}
                         />
                         <InputGroup.Append>
                             <InputGroup.Text id="basic-addon2">kg.</InputGroup.Text>
@@ -94,17 +108,19 @@ export const SearchVehicleNew = () => {
                         <FormControl
                             aria-label="Default"
                             aria-describedby="inputGroup-sizing-default"
+                            onChange={(e) => vehicleStore.searchRange = e.target.value}
                         />
                         <InputGroup.Append>
                             <InputGroup.Text id="basic-addon2">km.</InputGroup.Text>
                         </InputGroup.Append>
                     </InputGroup>
 
-                    <Button style={{width: '100%'}}>Search, but I dont do anything :) </Button>
-
                 </Col>
-                <Col xs={8}>{cardArray}</Col>
+                <Col xs={8}>{vehicleStore.filteredVehicles.map(vehicle =>
+                        /*Here should be a filter to only insert the cards passing the filter into the cardArray*/
+                        <CarCard Car={vehicle}/>
+                )}</Col>
             </Row>
         </Container>
     )
-}
+})
--- web/src/stores/VehicleStore.js
+++ web/src/stores/VehicleStore.js
@@ -1,6 +1,11 @@
-import {decorate, observable} from "mobx";
+import {computed, decorate, observable} from "mobx";
 
 class VehicleStore{
+    selectedBrands = [""];
+    searchName = "";
+    searchWeight = 0.0;
+    searchRange = 0.0;
+
     vehicles = [
         {brand: 'Volvo', name: '5-53x', price: '2500001', maxWeight: '696969', maxRange: '420420', fuelType: 'nutella', imageLink: 'https://f.nordiskemedier.dk/2gvdpo4v0qz2xyeb.jpg'},
         {brand: 'Ford', name: 'D-Linje', price: '1503000', maxWeight: '696969', maxRange: '420420', fuelType: 'mustard', imageLink: 'https://varebilogtransport.dk/wp-content/uploads/2019/05/Ford-F-Max-tyrkisk-truck-3_-1008x516.jpg'},
@@ -15,7 +20,8 @@ class VehicleStore{
         {brand: 'Ford', name: 'D-Linje', price: '1503000', maxWeight: '696969', maxRange: '420420', fuelType: 'mustard', imageLink: 'https://varebilogtransport.dk/wp-content/uploads/2019/05/Ford-F-Max-tyrkisk-truck-3_-1008x516.jpg'},
         {brand: 'Mercedes', name: 'Stor-20', price: '35900000', maxWeight: '696969', maxRange: '420420', fuelType: 'gas', imageLink: 'https://www.mercedesbenzcph.dk/lastbiler/wp-content/uploads/2018/11/18C0490_05022-2000x800.png'}];
 
-    brands = ["Volvo", "Ford", "Mercedes"]
+    brands = ["Volvo", "Ford", "Mercedes"];
+
 
     newBrand = "";
     newName = "";
@@ -24,7 +30,28 @@ class VehicleStore{
     newMaxRange = "";
     newFuelType = "";
     newImageLink = "";
+
+
+
+    get filteredVehicles(){
+        console.log("SearchWeight" + this.searchWeight);
+        return (
+            vehicleStore.vehicles.filter(vehicle =>
+                ((this.selectedBrands.length <=1 || this.selectedBrands.includes(vehicle.brand))
+                    && vehicle.name.includes(this.searchName)
+                    && ((this.searchWeight == 0.0) || Number(vehicle.maxWeight) <= Number(this.searchWeight))
+                    && ((this.searchRange == 0.0) || Number(vehicle.maxRange) <= Number(this.searchRange)))
+            )
+        )
+        //return this.vehicles
+    }
+
 }
+
+
+
+
+
 
 decorate(VehicleStore,{
     vehicles: observable,
@@ -34,7 +61,13 @@ decorate(VehicleStore,{
     newMaxWeight: observable,
     newMaxRange: observable,
     newFuelType: observable,
-    newImageLink: observable
+    newImageLink: observable,
+    selectedBrands: observable,
+    searchName: observable,
+    searchWeight: observable,
+    searchRange: observable,
+    filteredVehicles: computed,
+
 });
 
 export const vehicleStore = new VehicleStore();
