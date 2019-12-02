d4e0f5cdf3ccbe1e5c4b5ef70ffbacde78d64ad0
SimonJDTU <SimonJDTU@gmail.com>
2019-12-02 17:32:46 +0100

    Implemented mongoDB connector string

--- src/main/java/MongoConnector.java
+++ src/main/java/MongoConnector.java
@@ -7,12 +7,12 @@ import org.bson.Document;
 import java.util.ArrayList;
 
 public class MongoConnector {
-    private String username = System.getenv("MONGOUSER");
-    private String password = System.getenv("MONGOPASS");
+    private String username = System.getenv("DEVOPSMONGOUSER");
+    private String password = System.getenv("DEVOPSMONGOPASS");
     private String dbName = "test";
 
     public MongoDatabase getDb() {
-        MongoClient mongoClient = MongoClients.create("mongodb+srv://nofoss:"+password+"@nofoss-fokpd.mongodb.net/admin?retryWrites=true&w=majority");
+        MongoClient mongoClient = MongoClients.create("mongodb+srv://" + username + ":" + password + "@nofoss-d02td.mongodb.net/test?retryWrites=true&w=majority");
         return mongoClient.getDatabase(dbName);
     }
 
