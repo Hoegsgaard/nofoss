cb5b65f795935326780307753ef3e26fa7c146eb
hogsgaard <brejning16>
2019-12-03 16:13:51 +0100

    Login med google all done!

---
+++ src/main/webapp/precache-manifest.2d134ed39b6d6d11b6533d443804ac9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1490acf8cc06e55ff619c95a14ef9975",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b0e8a0f466f6cabcb3b",
+    "url": "/static/css/main.f4b59fc7.chunk.css"
+  },
+  {
+    "revision": "a92c5ab052fd84509345",
+    "url": "/static/js/2.1427f551.chunk.js"
+  },
+  {
+    "revision": "0b0e8a0f466f6cabcb3b",
+    "url": "/static/js/main.14fee185.chunk.js"
+  },
+  {
+    "revision": "faf3d913dea1519336c1",
+    "url": "/static/js/runtime~main.0b761a9c.js"
+  }
+]);
