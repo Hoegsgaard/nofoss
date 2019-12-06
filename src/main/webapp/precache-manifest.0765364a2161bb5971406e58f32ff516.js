d6c846968f1af210b4a80ed45861c04dc18575c0
hogsgaard <brejning16>
2019-12-06 16:01:10 +0100

    Klar til heroku! Final aflevering

---
+++ src/main/webapp/precache-manifest.0765364a2161bb5971406e58f32ff516.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66e3ee49bd082bb313020a6c922d86d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "781a09158e8a4be86bf1",
+    "url": "/static/css/main.f4b59fc7.chunk.css"
+  },
+  {
+    "revision": "ff175a0dbe5a0b8e073f",
+    "url": "/static/js/2.55dd535e.chunk.js"
+  },
+  {
+    "revision": "781a09158e8a4be86bf1",
+    "url": "/static/js/main.0fe32f87.chunk.js"
+  },
+  {
+    "revision": "faf3d913dea1519336c1",
+    "url": "/static/js/runtime~main.0b761a9c.js"
+  }
+]);
