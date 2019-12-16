0d1c9a3e0797f5e205774e4bee8452450e4cff67
SimonJDTU <SimonJDTU@gmail.com>
2019-12-16 14:25:46 +0100

    eksamen test

---
+++ src/main/webapp/precache-manifest.15d080c0a70b1d2e79cc492f524a7490.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e21251db40986de983829532620a200b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6907d41b0a4436e4cf7",
+    "url": "/static/css/main.d9d55b34.chunk.css"
+  },
+  {
+    "revision": "84267c6aff09b8c0464a",
+    "url": "/static/js/2.d7d26284.chunk.js"
+  },
+  {
+    "revision": "e6907d41b0a4436e4cf7",
+    "url": "/static/js/main.a075783f.chunk.js"
+  },
+  {
+    "revision": "faf3d913dea1519336c1",
+    "url": "/static/js/runtime~main.0b761a9c.js"
+  }
+]);
